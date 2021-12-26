292ad2248dbf2247cca24cbf7a5ac2c59b9d72b1
Hadrien Meyer <meyerhadrien96@gmail.com>
2021-12-26 22:19:35 +0100

    remove footer too

--- meal-choser.py
+++ meal-choser.py
@@ -78,6 +78,7 @@ if st.button('Find Place'):
 hide_menu_style = """
         <style>
         #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)

a13ac3670e2c85358f39bc49bb12feb3c3b8b9fe
Hadrien Meyer <meyerhadrien96@gmail.com>
2021-12-27 22:15:35 +0100

    Finish Type options, add reading file to cache

--- meal-choser.py
+++ meal-choser.py
@@ -4,37 +4,46 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-# Get Sheet From Google API
-r = requests.get(
-    'https://sheets.googleapis.com/v4/spreadsheets/1aEK8c3xDDAetlMKmC11r37D82KRaq73bP7VGMgvETTU/values/sheet1?valueRenderOption=FORMATTED_VALUE&key=AIzaSyB1LRyMMyhUzs7ac8keUcBdBpEEHD-Zni8'
-    ) 
-
-# Convert bytes to dict and load as DataFrame
-dictionnary = ast.literal_eval(r.content.decode('utf-8'))
-df = pd.DataFrame(dictionnary['values'])
-headers = df.iloc[0] 
-new_df  = pd.DataFrame(df.values[1:], columns=headers) 
-
-# Convert Columns Types
-new_df['Distance'] = new_df['Distance'].astype(float)
-
-
 bretzel_icon = "https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/pretzel_1f968.png"
 
 # Set page title and favicon.
 st.set_page_config(
     page_title="MealChooser", page_icon=bretzel_icon,
 )
+st.markdown("<h1 style='text-align: center; color: #FFFFFF;'>MealChooser</h1>",
+            unsafe_allow_html=True)
 
 
-st.title('MealChooser')
+@st.cache
+def load_data(r):
+    # Convert bytes to dict and load as DataFrame
+    dictionnary = ast.literal_eval(r.content.decode('utf-8'))
+    original_df = pd.DataFrame(dictionnary['values'])
+    headers = original_df.iloc[0]
+    df = pd.DataFrame(original_df.values[1:], columns=headers)
+
+    # Convert Columns Types
+    df['Distance'] = df['Distance'].astype(float)
+    return df
+
+@st.cache
+def filter_genre(df, genre):
+    df = df[new_df['Warm/Cold'] == genre]
+    return df
+
+# Get Sheet From Google API
+r = requests.get(
+    'https://sheets.googleapis.com/v4/spreadsheets/1aEK8c3xDDAetlMKmC11r37D82KRaq73bP7VGMgvETTU/values/sheet1?valueRenderOption=FORMATTED_VALUE&key=AIzaSyB1LRyMMyhUzs7ac8keUcBdBpEEHD-Zni8'
+    ) 
+
+# Convert bytes to dict and load as DataFrame
+new_df = load_data(r)
 
 # Filter Genre
 genre = st.radio(
     "Meal Type",
     ('Warm', 'Cold'))
 new_df = new_df[new_df['Warm/Cold'] == genre]
-
 
 
 # Filter Here or Togo
@@ -52,12 +61,11 @@ dist_min = float(new_df['Distance'].min())
 dist_max = float(new_df['Distance'].max())
 if dist_max != dist_min:
     values = st.slider(
-        'Distance (km)',
+        'Distance Time (min)',
         dist_min, dist_max, (dist_min, dist_max))
     new_df = new_df[new_df['Distance'] >= values[0]]
     new_df = new_df[new_df['Distance'] <= values[1]]
 
-    
 
 # Filter type of food
 food_options = np.unique(new_df['Type'])
@@ -65,6 +73,17 @@ if len(food_options) > 1:
     options = st.multiselect(
         'Food Kind',
         food_options)
+    if len(options) > 0:
+        new_df = new_df[new_df['Type'].isin(options)]
+
+# Remove unwanted places
+name_options = np.unique(new_df['Name'])
+if len(name_options) > 1:
+    unwanted = st.multiselect(
+    'Unwanted Places',
+    name_options)
+    if len(unwanted) > 0:
+        new_df = new_df[~new_df['Name'].isin(unwanted)]
 
 if st.button('Find Place'):
     if len(new_df) > 1:
